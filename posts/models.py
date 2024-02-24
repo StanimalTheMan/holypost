858404c9aab23852a07cba47da5188211e7bbb53
Stan Choi <sjc591@nyu.edu>
2024-02-24 15:18:30 -0500

    register posts app, run migration w  initial model, generate requirements.txt

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    title = models.CharField(max_length=255, null=True, blank=True)
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='images/')
-    moderated = models.BooleanField(defualt=False)
+    description = models.TextField()
+    moderated = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Post {self.id}'
